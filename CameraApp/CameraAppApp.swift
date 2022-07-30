8c2d92b050e12c76023d3c02dc6712c1405a3ebc
H-NGT <nov.13.v3.0@gmail.com>
2022-07-30 21:26:29 +0900

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by 永田浩之 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
